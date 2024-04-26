e205f10f8d110811ee9af7d92c0f0487e43fe08f
axterrr <vgibskiy@gmail.com>
2024-04-26 04:30:23 +0300

    cashier can see info about himself

--- src/main/java/kma/databases/controller/commands/employee/AllEmployeesCommand.java
+++ src/main/java/kma/databases/controller/commands/employee/AllEmployeesCommand.java
@@ -4,14 +4,19 @@ import kma.databases.constants.Attribute;
 import kma.databases.constants.Page;
 import kma.databases.controller.commands.Command;
 import kma.databases.controller.utils.HttpWrapper;
+import kma.databases.controller.utils.SessionManager;
 import kma.databases.entities.CustomerCard;
 import kma.databases.entities.Employee;
+import kma.databases.entities.Role;
 import kma.databases.services.CustomerCardService;
 import kma.databases.services.EmployeeService;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class AllEmployeesCommand implements Command {
 
@@ -23,8 +28,21 @@ public class AllEmployeesCommand implements Command {
 
     @Override
     public String execute(HttpWrapper httpWrapper) throws ServletException, IOException {
-        List<Employee> employees = employeeService.getAllEmployees();
-        httpWrapper.getRequest().setAttribute(Attribute.EMPLOYEES, employees);
+
+        Employee loggedInUser = SessionManager.getInstance().getUserFromSession(httpWrapper.getRequest().getSession());
+        List<Employee> empls = new ArrayList<>();
+
+        empls = employeeService.getAllEmployees();
+
+        /*if (loggedInUser.getRole().equals(Role.MANAGER)) {
+            empls = employeeService.getAllEmployees();
+        } else if (loggedInUser.getRole().equals(Role.CASHIER)) {
+            Optional<Employee> cashier = employeeService.getEmployeeById(loggedInUser.getId());
+            if(cashier.isPresent())
+                empls.add(cashier.get());
+        }*/
+
+        httpWrapper.getRequest().setAttribute(Attribute.EMPLOYEES, empls);
         return Page.ALL_EMPLOYEES_VIEW;
     }
 }
